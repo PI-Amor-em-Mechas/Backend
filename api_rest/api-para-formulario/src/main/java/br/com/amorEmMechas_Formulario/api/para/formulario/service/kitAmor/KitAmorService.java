a2158ea73ad726d39af58c2553efd8c25a2f9e00
felipzp <felipe.ferraz@sptech.school>
2026-04-03 11:17:08 -0300

    feat: adicionando rota para registrar e vincular kit do amor em algum paciente

--- api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/service/kitAmor/KitAmorService.java
+++ api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/service/kitAmor/KitAmorService.java
@@ -3,11 +3,13 @@ package br.com.amorEmMechas_Formulario.api.para.formulario.service.kitAmor;
 
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.kitAmor.KitAmorRequestDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.kitAmor.KitAmorResponseDto;
+import br.com.amorEmMechas_Formulario.api.para.formulario.entity.filho.Filho;
 import br.com.amorEmMechas_Formulario.api.para.formulario.entity.kitAmor.KitAmor;
 import br.com.amorEmMechas_Formulario.api.para.formulario.entity.paciente.Paciente;
 import br.com.amorEmMechas_Formulario.api.para.formulario.entity.solicitante.Solicitante;
 import br.com.amorEmMechas_Formulario.api.para.formulario.exception.IdNotFoundException;
 import br.com.amorEmMechas_Formulario.api.para.formulario.mapper.kitAmor.KitAmorMapper;
+import br.com.amorEmMechas_Formulario.api.para.formulario.repository.filho.FilhoRepository;
 import br.com.amorEmMechas_Formulario.api.para.formulario.repository.kitAmor.KitAmorRepository;
 import br.com.amorEmMechas_Formulario.api.para.formulario.repository.paciente.PacienteRepository;
 import br.com.amorEmMechas_Formulario.api.para.formulario.repository.solicitante.SolicitanteRepository;
@@ -21,6 +23,7 @@ public class KitAmorService {
     private KitAmorRepository repository;
     private SolicitanteRepository solicitanteRepository;
     private PacienteRepository pacienteRepository;
+
 
     public KitAmorService(KitAmorMapper mapper, PacienteRepository pacienteRepository, KitAmorRepository repository, SolicitanteRepository solicitanteRepository) {
         this.mapper = mapper;
@@ -38,6 +41,8 @@ public class KitAmorService {
                 .orElseThrow(()
                         ->new IdNotFoundException
                         ("ID PACIENTE: " + dto.getPacienteId() + " Não Encontrado"));
+
+
 
     KitAmor kitaAmor = mapper.toEntity(dto);
     kitaAmor.setSolicitante(solicitante);
