3ecf352fe394c81f29e89c09523f3805c4120154
Leonardo Monteiro <leonardo.monteiro@sptech.school>
2026-04-04 14:43:07 -0300

    add: Swagger configurado e implementado em todos os controllers

--- api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/kitAmor/KitAmorControlller.java
+++ api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/kitAmor/KitAmorControlller.java
@@ -1,30 +1,34 @@
 package br.com.amorEmMechas_Formulario.api.para.formulario.controller.kitAmor;
 
-
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.kitAmor.KitAmorRequestDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.kitAmor.KitAmorResponseDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.service.kitAmor.KitAmorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Kit do Amor", description = "Gerenciamento dos kits de peruca")
 @RestController
 @RequestMapping("/kits")
 @CrossOrigin(origins = "*")
 public class KitAmorControlller {
 
-
-    public KitAmorService service;
+    private KitAmorService service; // corrigido: era public
 
     public KitAmorControlller(KitAmorService service) {
         this.service = service;
     }
 
+    @Operation(summary = "Cadastra um novo kit do amor")
+    @ApiResponse(responseCode = "201", description = "Kit cadastrado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
     @PostMapping
     public ResponseEntity<KitAmorResponseDto> create(@RequestBody @Valid KitAmorRequestDto dto) {
         KitAmorResponseDto response = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
-
     }
 }
--- api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/paciente/PacienteController.java
+++ api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/paciente/PacienteController.java
@@ -1,15 +1,4 @@
-package br.com.amorEmMechas_Formulario.api.para.formulario.controller.paciente;
-
-
-import br.com.amorEmMechas_Formulario.api.para.formulario.dto.paciente.PacienteRequestDto;
-import br.com.amorEmMechas_Formulario.api.para.formulario.dto.paciente.PacienteResponseDto;
-import br.com.amorEmMechas_Formulario.api.para.formulario.mapper.paciente.PacienteMapper;
-import br.com.amorEmMechas_Formulario.api.para.formulario.service.paciente.PacienteService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "Pacientes", description = "Gerenciamento de pacientes")
 @RestController
 @RequestMapping("/pacientes")
 @CrossOrigin(origins = "*")
@@ -21,18 +10,21 @@ public class PacienteController {
         this.service = service;
     }
 
-
+    @Operation(summary = "Cadastra um novo paciente")
+    @ApiResponse(responseCode = "201", description = "Paciente criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
     @PostMapping
-    public ResponseEntity<PacienteResponseDto> save (@RequestBody @Valid PacienteRequestDto dto){
+    public ResponseEntity<PacienteResponseDto> save(@RequestBody @Valid PacienteRequestDto dto) {
         PacienteResponseDto response = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
-
     }
 
+    @Operation(summary = "Busca paciente por ID")
+    @ApiResponse(responseCode = "200", description = "Paciente encontrado")
+    @ApiResponse(responseCode = "404", description = "Paciente não encontrado")
     @GetMapping("/{id}")
-    public ResponseEntity<PacienteResponseDto> findById (@PathVariable Integer id){
+    public ResponseEntity<PacienteResponseDto> findById(@PathVariable Integer id) {
         PacienteResponseDto response = service.findById(id);
         return ResponseEntity.ok(response);
     }
-
 }
