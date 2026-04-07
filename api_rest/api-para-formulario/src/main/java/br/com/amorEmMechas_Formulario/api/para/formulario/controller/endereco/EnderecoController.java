794ccc95ab38e8980beb41ef00348030cf08c611
felipzp <felipe.ferraz@sptech.school>
2026-04-07 20:01:34 -0300

    feat: adicionando rota para atualizar endereço

--- api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/endereco/EnderecoController.java
+++ api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/controller/endereco/EnderecoController.java
@@ -2,11 +2,14 @@ package br.com.amorEmMechas_Formulario.api.para.formulario.controller.endereco;
 
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.endereco.EnderecoRequestDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.endereco.EnderecoResponseDto;
+import br.com.amorEmMechas_Formulario.api.para.formulario.dto.endereco.ViaCepResponseDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.service.endereco.EnderecoService;
+import br.com.amorEmMechas_Formulario.api.para.formulario.service.endereco.ViaCepService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.*;
 public class EnderecoController {
 
     private EnderecoService service;
+
+    @Autowired
+    private ViaCepService viaCepService;
 
     public EnderecoController(EnderecoService service) {
         this.service = service;
@@ -31,4 +37,30 @@ public class EnderecoController {
         EnderecoResponseDto response = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @Operation(summary = "Cadastra um novo endereço")
+    @ApiResponse(responseCode = "201", description = "Endereço cadastrado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    @PostMapping("/viacep")
+    public ViaCepResponseDto preencherEndereco(@RequestBody EnderecoRequestDto request) {
+        return viaCepService.buscarEnderecoPorCep(request.getCep());
+    }
+
+    @Operation(summary = "Atualizar um endereço")
+    @ApiResponse(responseCode = "200", description = "Endereço atualizado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos")
+    @PutMapping("/{id}")
+    public ResponseEntity<EnderecoResponseDto> update (@PathVariable Integer id, @RequestBody @Valid EnderecoRequestDto dto){
+        EnderecoResponseDto response = service.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
 }
+
+
+
+
+
+
+
--- api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/service/endereco/EnderecoService.java
+++ api_rest/api-para-formulario/src/main/java/br/com/amorEmMechas_Formulario/api/para/formulario/service/endereco/EnderecoService.java
@@ -4,6 +4,7 @@ package br.com.amorEmMechas_Formulario.api.para.formulario.service.endereco;
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.endereco.EnderecoRequestDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.dto.endereco.EnderecoResponseDto;
 import br.com.amorEmMechas_Formulario.api.para.formulario.entity.endereco.Endereco;
+import br.com.amorEmMechas_Formulario.api.para.formulario.exception.IdNotFoundException;
 import br.com.amorEmMechas_Formulario.api.para.formulario.mapper.endereco.EnderecoMapper;
 import br.com.amorEmMechas_Formulario.api.para.formulario.repository.endereco.EnderecoRepository;
 import org.springframework.stereotype.Service;
@@ -23,11 +24,29 @@ public class EnderecoService {
     }
 
 
-
     public EnderecoResponseDto create (EnderecoRequestDto dto){
         Endereco e = mapper.toEntity(dto);
         Endereco saved = repository.save(e);
         return mapper.toResponse(e);
+    }
+
+    public EnderecoResponseDto update (Integer id, EnderecoRequestDto dto){
+        Endereco e = repository.findById(id).orElseThrow(() -> new IdNotFoundException("ID: " + id + " Não Encontrado"));
+
+
+            e.setRua(dto.getRua());
+            e.setBairro(dto.getBairro());
+            e.setCep(dto.getCep());
+            e.setCidade(dto.getCidade());
+            e.setEstado(dto.getEstado());
+            e.setNumero(dto.getNumero());
+
+            Endereco endereco = repository.save(e);
+            return mapper.toResponse(endereco);
+
+
+
+
     }
 
 }
